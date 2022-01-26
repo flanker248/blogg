9b23ab8cdf4b5d6f78f6cf92ecc2bb5ec214d332
Chirag Bhasin <chiragbhasin248@gmail.com>
2022-01-26 15:50:56 +0530

    Redirecting home to list page

--- src/main/java/com/cbyk/blogg/controller/BlogController.java
+++ src/main/java/com/cbyk/blogg/controller/BlogController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.ZonedDateTime;
 import java.util.Map;
@@ -24,8 +26,9 @@ public class BlogController {
     BlogService blogService;
 
     @GetMapping("/")
-    public String greeting(Model model) {
-        return "blog_list";
+    public String greeting(ModelMap model) {
+        return "redirect:list";
+
     }
 
     @GetMapping("/about")
