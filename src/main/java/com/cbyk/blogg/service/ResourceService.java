b39f0f12c24a1b7f2056c839991fe7b1759b026e
Chirag Bhasin <chiragbhasin248@gmail.com>
2022-01-27 20:07:59 +0530

    using spring boot caching for serving resources

--- src/main/java/com/cbyk/blogg/service/ResourceService.java
+++ src/main/java/com/cbyk/blogg/service/ResourceService.java
@@ -4,6 +4,7 @@ import com.cbyk.blogg.config.MyCache;
 import com.cbyk.blogg.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class ResourceService {
 
     public static String IMG_PATH_PREFIX = "static/assets/img/";
 
+    @Cacheable(value="resources")
     public ClassPathResource fetchResource(String name, String type) {
         String appender = "";
         ClassPathResource classPathResource = null;
@@ -21,10 +23,6 @@ public class ResourceService {
             appender = IMG_PATH_PREFIX;
 
         StringBuilder fullPathSB = new StringBuilder().append(appender).append(name);
-        classPathResource = (ClassPathResource) MyCache.fetch(fullPathSB.toString());
-        if (classPathResource != null) {
-            return classPathResource;
-        }
 
         try {
             classPathResource = new ClassPathResource(fullPathSB.toString());
@@ -32,7 +30,6 @@ public class ResourceService {
             System.out.println("ClassPathResource not found at path:");
             return null;
         }
-        MyCache.store(fullPathSB.toString(), classPathResource);
         return classPathResource;
 
     }
